6c5f3f6fe4e3e9da3595932eddefdb10771a345f
Sebastian <sebastian.matyjas@gmail.com>
2016-01-19 20:59:06 +0100

    Początek pliku Pracownik.java
    Początek pliku Pracownik.java (kino/domain)

---
+++ src/main/java/com/kino/domain/Pracownik.java
@@ -0,0 +1,7 @@
+package com.kino.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
