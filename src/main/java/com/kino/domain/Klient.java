f848fe50657e2456e0085de8f54b9f8c81c97794
Sebastian <sebastian.matyjas@gmail.com>
2016-01-19 20:50:34 +0100

    Skończony plik Klient.java
    Skończony plik Klient.java na podstawie pliku Car.java

--- src/main/java/com/kino/domain/Klient.java
+++ src/main/java/com/kino/domain/Klient.java
@@ -9,5 +9,67 @@ import java.util.List;
 @NamedQueries({
         @NamedQuery(name = "klient.pobierzWszystkich", query = "Select k from Klient k"),
         @NamedQuery(name = "klient.pobierzPoID", query = "Select k from Klient k where k.idKlient = :idKlient"),
-        @NamedQuery(name = "klient.kartapromocyjna", query = "Select k from Klient k where k.kartapromocyjna = true")
+        @NamedQuery(name = "klient.kartaPromocyjna", query = "Select k from Klient k where k.kartaPromocyjna = true")
 })
+
+public class Klient {
+    private Long idKlient;
+    private String Imie;
+    private String Nazwisko;
+    private String nrTelefonu;
+    private Boolean kartaPromocyjna = false;
+
+    private List<Zamowienie> zamowienia = new ArrayList<Zamowienie>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getidKlient() {
+        return idKlient;
+    }
+	
+    public void setidKlient(Long idKlient) {
+        this.idKlient = idKlient;
+    }
+
+    public String getImie() {
+        return Imie;
+    }
+    public void setImie(String Imie) {
+        this.Imie = Imie;
+    }
+
+    public String getNazwisko() {
+        return Nazwisko;
+    }
+	
+    public void setNazwisko(String Nazwisko) {
+        this.Nazwisko = Nazwisko;
+    }
+
+    public String getnrTelefonu() {
+        return nrTelefonu;
+    }
+	
+    public void setnrTelefonu(String nrTelefonu) {
+        this.nrTelefonu = nrTelefonu;
+    }
+
+    public Boolean getkartaPromocyjna() {
+        return kartaPromocyjna;
+    }
+	
+    public void setkartaPromocyjna(Boolean kartaPromocyjna) {
+        this.kartaPromocyjna = kartaPromocyjna;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "klient")
+    @JsonIgnore
+    public List<Zamowienie> getZamowienia() {
+        return zamowienia;
+    }
+	
+    public void setZamowienia(List<Zamowienie> zamowienia) {
+        this.zamowienia = zamowienia;
+    }
+	
+}
